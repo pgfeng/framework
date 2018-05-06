7f6040f0e5093eefcd4ebfa5051a02f00be27339
PGF <592472116@qq.com>
2018-05-06 10:42:32 +0800

    调整模板引擎

--- src/Hooks/GFPHP.php
+++ src/Hooks/GFPHP.php
@@ -63,7 +63,7 @@ class GFPHP
 
         //当为空时只需
         $patterns[] = "/" . $leftDelim . "elseLoop" . $rightDelim . "/i";
-        $replaces[] = "<?php } \\n else { ?>";
+        $replaces[] = "<?php } \\\\\\n else { ?>";
 
 
         $patterns[] = "/" . $leftDelim . "elseif\s+(.+?)" . $rightDelim . "/";
